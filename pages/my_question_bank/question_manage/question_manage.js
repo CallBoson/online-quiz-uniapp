6f2b280c80cf482afe4a9c5f1dccc7dcd31b9c4a
饿了 <9441896+bosons@user.noreply.gitee.com>
2023-03-31 17:05:46 +0800

    feat：题目管理

--- pages/my_question_bank/question_manage/question_manage.js
+++ pages/my_question_bank/question_manage/question_manage.js
@@ -1,9 +1,20 @@
+let originQuestions = []
 export default {
     data() {
         return {
-            question_bank_name: '',
-            owner_info: {},
-            questions: []
+            question_bank_name: '', // 题库名称
+            owner_info: {}, // 题库创建者信息
+            questions: [], // 题目列表
+            showTypeSelect: false, // 是否显示题目类型选择
+            typeList: [
+                { label: '单选题', value: '1' },
+                { label: '多选题', value: '2' },
+                { label: '判断题', value: '3' },
+                { label: '填空题', value: '4' },
+                { label: '简答题', value: '5' },
+            ], // 题目类型列表
+            currentSelectType: undefined, // 当前选择的题目类型
+            isShowDetail: false, // 是否显示题目详情
         }
     },
     onLoad(options) {
@@ -12,13 +23,104 @@ export default {
         }
     },
     methods: {
+        // 获取题库详情
         getDetail(id) {
             uni.post('/questionBank/questionManage', { id }).then(res => {
                 console.log(res)
                 this.question_bank_name = res.data.question_bank_name
                 this.owner_info = res.data.owner
-                this.questions = res.data.questions
+                // 题目列表
+                this.questions = Array.from(res.data.questions, item => {
+                    if (item.question) {
+                        item.question = JSON.parse(item.question)
+                    }
+                    if (item.answer_single) {
+                        item.answer_single = JSON.parse(item.answer_single)
+                    }
+                    if (item.answer_multiple) {
+                        item.answer_multiple = JSON.parse(item.answer_multiple)
+                    }
+                    if (item.answer_judge) {
+                        item.answer_judge = JSON.parse(item.answer_judge)
+                    }
+                    if (item.answer_fill) {
+                        item.answer_fill = JSON.parse(item.answer_fill)
+                    }
+                    if (item.analysis) {
+                        item.analysis = JSON.parse(item.analysis)
+                    }
+                    return item
+                })
+                originQuestions = this.questions
             })
+        },
+        // 选择题目类型
+        confirmTypeSelect(value) {
+            this.currentSelectType = value[0]
+            this.questions = originQuestions.filter(item => {
+                return String(item.type) === this.currentSelectType.value
+            })
+        },
+        // 重置题目类型选择
+        resetSelectType() {
+            this.currentSelectType = undefined
+            this.questions = originQuestions
+        },
+        // 获取题目选项
+        getOptions(item) {
+            const type = String(item.type)
+            if (type === '1') {
+                return item.answer_single
+            } else if (type === '2') {
+                return item.answer_multiple
+            } else if (type === '3') {
+                return [
+                    { content: '正确' },
+                    { content: '错误' },
+                ]
+            } else {
+                return []
+            }
+        },
+        // 获取题目答案文本
+        getAnswerText(item) {
+            const type = String(item.type)
+            if (type === '1') {
+                return item.answer_single.find(item => item.isAnswer).content
+            } else if (type === '2') {
+                return Array.from(item.answer_multiple.filter(item => item.isAnswer), obj => {
+                    return obj.content
+                }).join('；')
+            } else if (type === '3') {
+                return item.answer_judge ? '正确' : '错误'
+            } else if (type === '4') {
+                let text = '\n'
+                for (let i = 0; i < item.answer_fill.length; i++) {
+                    text += `空${i+1}: ${item.answer_fill[i].join('；')}\n`;
+                }
+                return text
+            } else {
+                return ''
+            }
+        }
+    },
+    filters: {
+        // 题目类型
+        questionTypeFilter(value) {
+            switch (String(value)) {
+                case '1':
+                    return '单选题'
+                case '2':
+                    return '多选题'
+                case '3':
+                    return '判断题'
+                case '4':
+                    return '填空题'
+                case '5':
+                    return '简答题'
+                default:
+                    return '未知类型'
+            }
         }
     }
 }
--- pages/my_question_bank/question_manage/question_manage.vue
+++ pages/my_question_bank/question_manage/question_manage.vue
@@ -9,7 +9,62 @@
 				<view class="question-count">总题数：{{ questions.length }}题</view>
 			</view>
 			<input class="search-wrap" placeholder="输入题库关键字搜搜" />
+			<view class="type-wrap">
+				<view class="type-select" @click="showTypeSelect = true">
+					{{ currentSelectType ? currentSelectType.label : '全部类型' }}
+					<text class="tn-icon-down"></text>
+				</view>
+				<view class="unfold" @click="isShowDetail = !isShowDetail">
+					<text class="tn-icon-eye"></text>
+					{{ isShowDetail ? '收起' : '展开' }}详情
+				</view>
+			</view>
 		</view>
+
+		<view class="content-wrap">
+			<tn-empty v-if="questions.length === 0" mode="list" style="margin-top: 100rpx;"></tn-empty>
+			<view v-else class="question-list">
+				<view v-for="(item, index) in questions" :key="item.id" class="question-item">
+					<!-- 题目类型 -->
+					<view class="question-info-wrap">
+						<view class="question-type">{{ item.type | questionTypeFilter }}</view>
+						<view class="icons-wrap">
+							<text class="tn-icon-edit"></text>
+							<text class="tn-icon-delete"></text>
+						</view>
+					</view>
+					<!-- 题目 -->
+					<view class="question-content-wrap">{{ index + 1 }}.{{ item.question.content }}</view>
+					<!-- 题目详情 -->
+					<view class="question-detail" v-if="isShowDetail">
+						<view class="options-wrap">
+							<view class="option-item" v-for="(option, index) in getOptions(item)" :key="option.id">
+								<view class="option-index">{{ String.fromCharCode(65 + index) }}.</view>
+								<view class="option-content">{{ option.content }}</view>
+							</view>
+						</view>
+						<text class="question-answer">
+							<text style="color: darkgray;margin-right: 10rpx;">[答案]</text>
+							{{ getAnswerText(item) }}
+						</text>
+						<view v-if="item.analysis" class="question-analysis">
+							<text style="color: darkgray;margin-right: 10rpx;">[解析]</text>
+							{{ item.analysis.content }}
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+
+		<view class="bottom-wrap">
+			<view class="btn-wrap">
+				<!-- <tn-button width="320rpx" height="80rpx" :fontSize="32">批量整理</tn-button> -->
+				<tn-button width="500rpx" height="80rpx" :fontSize="32" backgroundColor="#5fa3fb" fontColor="#FFF">添加题目</tn-button>
+			</view>
+		</view>
+
+		<!-- 选择题型 -->
+		<tn-select v-model="showTypeSelect" @cancel="resetSelectType" mode="single" cancelText="重置" :list="typeList" @confirm="confirmTypeSelect"></tn-select>
 	</view>
 </template>
 
